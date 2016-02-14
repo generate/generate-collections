3f71ee637f11e7492906b3922daed50f41492c8a
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-14 18:46:46 -0500

    100% coverage

--- test.js
+++ test.js
@@ -21,15 +21,41 @@ describe('generate-collections', function() {
     });
   });
 
-  describe('load', function() {
-    describe('collections', function() {
-      it('should add collections to the instance', function(cb) {
-        collections.invoke(app);
+  describe('collections', function() {
+    it('should add collections to the instance', function(cb) {
+      collections.invoke(app);
 
-        assert.equal(typeof app.docs, 'function');
-        assert.equal(typeof app.includes, 'function');
-        assert.equal(typeof app.layouts, 'function');
-        cb();
+      assert.equal(typeof app.docs, 'function');
+      assert.equal(typeof app.includes, 'function');
+      assert.equal(typeof app.layouts, 'function');
+      cb();
+    });
+
+    it('should create custom collections passed on invoke options', function(cb) {
+      app.generator('foo', function(foo) {
+        collections.invoke(this, {
+          create: {
+            snippet: { viewType: 'partial' },
+            section: { viewType: 'partial' },
+            block: { viewType: 'layout' }
+          }
+        });
+        var count = 0;
+
+        foo.task('bar', function(next) {
+          assert(foo.views.hasOwnProperty('snippets'));
+          assert(foo.views.hasOwnProperty('sections'));
+          assert(foo.views.hasOwnProperty('blocks'));
+          count++;
+          next();
+        });
+
+        foo.task('default', ['bar']);
+        foo.build('default', function(err) {
+          if (err) return cb(err);
+          assert.equal(count, 1);
+          cb();
+        });
       });
     });
   });
@@ -55,13 +81,6 @@ describe('generate-collections', function() {
           cb();
         });
       });
-    });
-  });
-
-  describe('function', function() {
-    it('', function(cb) {
-
-      cb();
     });
   });
 });
